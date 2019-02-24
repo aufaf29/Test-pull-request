522fdd197b3d9910a8556cbdcfd5f06874bd077d
Wolfhysics <farreldzaudan2204@gmail.com>
2019-02-24 15:41:38 +0700

    terakhir

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main(){
-
 	std::cout<<"Halo saya Dzaudan"<<std::endl;
 	std::cout<<"Senang berkenalan denganmu"<<std::endl;
 	std::cout<<"senang berkenalan juga"<<std::endl;
