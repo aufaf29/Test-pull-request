3b99225c71b02c085d2045e61ce17a5ed84f071a
Wolfhysics <farreldzaudan2204@gmail.com>
2019-02-24 15:28:35 +0700

    FDN 004 revisi

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout<<"Wolfhysics here, Farrel D N 16718004"<<std::endl;
+	std::cout<<"Halo saya Farrel DN"<<std::endl;
+	std::cout<<"Senang berkenalan denganmu"<<std::endl;
 	return 0;
 }
